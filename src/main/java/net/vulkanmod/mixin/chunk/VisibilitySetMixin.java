b0ecdd147c645420ff7cb82146d8f51bddeb8a8a
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-01-02 20:13:53 -0300

    Update VisibilitySetMixin.java

--- src/main/java/net/vulkanmod/mixin/chunk/VisibilitySetMixin.java
+++ src/main/java/net/vulkanmod/mixin/chunk/VisibilitySetMixin.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.Overwrite;
 @Mixin(VisibilitySet.class)
 public class VisibilitySetMixin implements VisibilitySetExtended {
 
-//    private int vis2 = 0;
     private long vis = 0;
 
     /**
@@ -18,9 +17,9 @@ public class VisibilitySetMixin implements VisibilitySetExtended {
      */
     @Overwrite
     public void set(Direction dir1, Direction dir2, boolean p_112989_) {
-//        this.vis |= 1 << (dir1.ordinal() * 5 + dir2.ordinal()) | 1 << (dir2.ordinal() * 5 + dir1.ordinal());
-        this.vis |= 1L << ((dir1.ordinal() << 3) + dir2.ordinal()) | 1L << ((dir2.ordinal() << 3) + dir1.ordinal());
-//        this.vis2 |= 1L << ((dir1.ordinal() >> 1 << 3) + (dir2.ordinal() >> 1));
+        long visBit = 1L << ((dir1.ordinal() << 3) + dir2.ordinal());
+        this.vis &= ~visBit;
+        this.vis |= visBit;
     }
 
     /**
@@ -29,7 +28,7 @@ public class VisibilitySetMixin implements VisibilitySetExtended {
      */
     @Overwrite
     public void setAll(boolean bl) {
-        if(bl) this.vis = 0xFFFFFFFFFFFFFFFFL;
+        if (bl) this.vis = 0xFFFFFFFFFFFFFFFFL;
     }
 
     /**
@@ -38,12 +37,6 @@ public class VisibilitySetMixin implements VisibilitySetExtended {
      */
     @Overwrite
     public boolean visibilityBetween(Direction dir1, Direction dir2) {
-//        return (this.vis & (1 << (dir1.ordinal() * 5 + dir2.ordinal()))) != 0;
         return (this.vis & (1L << ((dir1.ordinal() << 3) + dir2.ordinal()))) != 0;
-    }
-
-    @Override
-    public long getVisibility() {
-        return vis;
     }
 }
